c6d7d06a27929822f04d8853f1f077cca4573751
Naing Paing Oo <naingpaingoo@gmail.com>
2024-10-07 14:54:54 +0630

    first commit

---
+++ my_id_card/lib/main.dart
@@ -0,0 +1,147 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyIdCard(),
+  ));
+}
+
+class MyIdCard extends StatefulWidget {
+  const MyIdCard({super.key});
+
+  @override
+  State<MyIdCard> createState() => _MyIdCardState();
+}
+
+class _MyIdCardState extends State<MyIdCard> {
+  int myLevel = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: const Text(
+          'My ID Card',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        setState(() {
+          if(myLevel >= 5){
+            myLevel = 5;
+          }else{
+            myLevel += 1;
+          }
+        });
+      },
+      child: Icon(
+        Icons.add,
+        color: Colors.grey[800],
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('images/mnpo.jpg'),
+                radius: 60.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey,
+            ),
+            Text('NAME',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                )),
+            SizedBox(height: 10.0),
+            Text('Min Naing Paing Oo',
+                style: TextStyle(
+                  color: Colors.amber,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(height: 30.0),
+            Text('POSITION',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                )),
+            SizedBox(height: 10.0),
+            Text('Mobile Application Developer',
+                style: TextStyle(
+                  color: Colors.amber,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(height: 30.0),
+            Text('CURRENT TECHNICAL LEVEL',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                )),
+            SizedBox(height: 10.0),
+            Text('$myLevel',
+                style: TextStyle(
+                  color: Colors.amber,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(height: 30.0),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey,
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'naingpaingoo@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              children: [
+                Icon(
+                  Icons.phone,
+                  color: Colors.grey,
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  '+959790663667',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
